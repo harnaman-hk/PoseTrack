dc9f22fa9f53d3ab6f44e58d687c3003141f48d2
harnaman-hk <harnaman.hk@gmail.com>
2021-05-22 19:49:28 +0530

    link bg.js with frontend

--- data/interface/index.js
+++ data/interface/index.js
@@ -1,4 +1,5 @@
-// import * as tf from '@tensorflow/tfjs';
+import { processFrames } from '../../processor.js';
+
 var config = {
     "count": 0,
     "data": [],
@@ -86,7 +87,22 @@ var config = {
     }
 };
 
-var load = async function() {
+async function processStream() {
+    imgObj = document.getElementById("camera");
+    const webcam = await tf.data.webcam(imgObj);
+    modeljson = { "modelTopology": { "class_name": "Sequential", "config": { "name": "sequential_2", "layers": [{ "class_name": "Dense", "config": { "units": 100, "activation": "relu", "use_bias": true, "kernel_initializer": { "class_name": "VarianceScaling", "config": { "scale": 1, "mode": "fan_in", "distribution": "normal", "seed": null } }, "bias_initializer": { "class_name": "Zeros", "config": {} }, "kernel_regularizer": null, "bias_regularizer": null, "activity_regularizer": null, "kernel_constraint": null, "bias_constraint": null, "name": "dense_Dense1", "trainable": true, "batch_input_shape": [null, 14739], "dtype": "float32" } }, { "class_name": "Dropout", "config": { "rate": 0.5, "noise_shape": null, "seed": null, "name": "dropout_Dropout1", "trainable": true } }, { "class_name": "Dense", "config": { "units": 3, "activation": "softmax", "use_bias": false, "kernel_initializer": { "class_name": "VarianceScaling", "config": { "scale": 1, "mode": "fan_in", "distribution": "normal", "seed": null } }, "bias_initializer": { "class_name": "Zeros", "config": {} }, "kernel_regularizer": null, "bias_regularizer": null, "activity_regularizer": null, "kernel_constraint": null, "bias_constraint": null, "name": "dense_Dense2", "trainable": true } }] }, "keras_version": "tfjs-layers 1.3.1", "backend": "tensor_flow.js" }, "weightsManifest": [{ "paths": ["weights.bin"], "weights": [{ "name": "dense_Dense1/kernel", "shape": [14739, 100], "dtype": "float32" }, { "name": "dense_Dense1/bias", "shape": [100], "dtype": "float32" }, { "name": "dense_Dense2/kernel", "shape": [100, 3], "dtype": "float32" }] }] };
+    modelmeta = { "tfjsVersion": "1.3.1", "tmVersion": "0.8.6", "packageVersion": "0.8.6", "packageName": "@teachablemachine/pose", "timeStamp": "2020-11-21T18:30:15.108Z", "userMetadata": {}, "modelName": "my-pose-model", "labels": ["good posture", "bad posture", "near screen"], "modelSettings": { "posenet": { "architecture": "MobileNetV1", "outputStride": 16, "inputResolution": 257, "multiplier": 0.75 } } };
+    // try {
+
+    //     const model = await tf.loadLayersModel(modeljson);
+    // } catch (err) {
+    //     console.log(err);
+    // }
+    // console.log("webcam");
+    // console.log(webcam);
+}
+
+var load = function() {
     var start = document.getElementById("start");
     var player = document.getElementById("camera");
     var cancel = document.getElementById("cancel");
@@ -106,12 +122,10 @@ var load = async function() {
                 player.play();
 
                 // work on prediction
-                // console.log(config.stream.getTracks());
-                while (start.disabled) {
-                    imgObj = document.getElementById("camera");
-                    const webcam = await tf.data.webcam(imgObj);
-                    console.log(webcam);
-                }
+                // imgObj = document.getElementById("camera");
+                // const webcam = await tf.data.webcam(imgObj);
+                // console.log(webcam);
+
 
             }).catch(function(e) {});
         } else console.error("navigator.mediaDevices is not available!");
@@ -144,4 +158,77 @@ window.addEventListener("resize", function() {
     }, 1000);
 }, false);
 
-window.addEventListener("load", load, false);
+window.addEventListener("load", async() => {
+    load();
+    // processStream();
+    processFrames();
+}, false);
+// const webcamElement = document.getElementById('webcam');
+// const classifier = knnClassifier.create();
+
+// let net;
+
+// async function app() {
+//     console.log('Loading mobilenet..');
+
+//     // Load the model.
+//     net = await mobilenet.load();
+//     console.log('Successfully loaded model');
+
+//     // Create an object from Tensorflow.js data API which could capture image
+//     // from the web camera as Tensor.
+//     const webcam = await tf.data.webcam(webcamElement);
+
+//     // Reads an image from the webcam and associates it with a specific class
+//     // index.
+//     const addExample = async classId => {
+
+//         for (let x = 50; x > 0; x--) {
+//             // Capture an image from the web camera.
+//             const img = await webcam.capture();
+
+//             // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+//             // to the KNN classifier.
+//             const activation = net.infer(img, 'conv_preds');
+
+//             // Pass the intermediate activation to the classifier.
+//             classifier.addExample(activation, classId);
+
+//             // Dispose the tensor to release the memory.
+//             img.dispose();
+
+//             // Add some time between images so there is more variance
+//             setTimeout(() => {
+//                 console.log("Added image")
+//             }, 100)
+//         }
+//     };
+
+//     // When clicking a button, add an example for that class.
+//     document.getElementById('class-a').addEventListener('click', () => addExample(0));
+//     document.getElementById('class-b').addEventListener('click', () => addExample(1));
+
+//     while (true) {
+//         if (classifier.getNumClasses() > 0) {
+//             const img = await webcam.capture();
+
+//             // Get the activation from mobilenet from the webcam.
+//             const activation = net.infer(img, 'conv_preds');
+//             // Get the most likely class and confidence from the classifier module.
+//             const result = await classifier.predictClass(activation);
+
+//             const classes = ['notouch', 'touch'];
+//             document.getElementById('console').innerText = `
+//         prediction: ${classes[result.label]}\n
+//         probability: ${result.confidences[result.label]}
+//       `;
+
+//             // Dispose the tensor to release the memory.
+//             img.dispose();
+//         }
+
+//         await tf.nextFrame();
+//     }
+// }
+
+// app();
---
+++ processor.js
@@ -0,0 +1,202 @@
+console.log("Ready");
+
+const URL = "./model/model.json";
+let model, webcam, ctx, labelContainer, maxPredictions;
+
+let group = [];
+let toggle = false;
+
+async function init() {
+    toggle = true;
+    const modelURL = URL;
+    const metadataURL = URL + "/metadata.json";
+
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // Note: the pose library adds a tmPose object to your window (window.tmPose)
+
+    model = await tmPose.load(modelURL, metadataURL);
+    // you need to create File objects, like with file input elements (<input type="file" ...>)
+    // const uploadModel = document.getElementById('upload-model');
+    // const uploadWeights = document.getElementById('upload-weights');
+    // const uploadMetadata = document.getElementById('upload-metadata');
+    // model = await tmPose.loadFromFiles(uploadModel.files[0], uploadWeights.files[0], uploadMetadata.files[0])
+    maxPredictions = model.getTotalClasses();
+
+    // Convenience function to setup a webcam
+    // let mql = window.matchMedia("(max-width: 570px)");
+    // const size = !mql ? window.innerWidth * 0.6 : window.innerHeight * 0.48;
+    // const flip = true; // whether to flip the webcam
+    // webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
+    // await webcam.setup(); // request access to the webcam
+    // await webcam.play();
+
+    imgObj = document.getElementById("camera");
+    const webcam = await tf.data.webcam(imgObj);
+    window.requestAnimationFrame(loop);
+
+    // // append/get elements to the DOM
+    // const canvas = document.getElementById("canvas");
+    // canvas.width = !mql ?
+    //     window.innerWidth * 0.5 :
+    //     window.innerHeight * 0.45;
+    // canvas.height = !mql ?
+    //     window.innerWidth * 0.5 :
+    //     window.innerHeight * 0.45;
+    // ctx = canvas.getContext("2d");
+    // labelContainer = document.getElementById("label-container");
+    // for (let i = 0; i < maxPredictions; i++) {
+    //     // and class labels
+    //     labelContainer.appendChild(document.createElement("div"));
+    // }
+}
+
+async function loop(timestamp) {
+    console.log(timestamp);
+    webcam.update(); // update the webcam frame
+
+    await predict();
+    if (toggle) {
+        window.requestAnimationFrame(loop);
+    }
+}
+
+async function predict() {
+    // Prediction #1: run input through posenet
+    // estimatePose can take in an image, video or canvas html element
+    const {
+        pose,
+        posenetOutput
+    } = await model.estimatePose(webcam.canvas);
+    // Prediction 2: run input through teachable machine classification model
+    const prediction = await model.predict(posenetOutput);
+    // console.log(group)
+    if (group.length < 100) {
+        group.push(prediction);
+    } else {
+        group.shift();
+        group.push(prediction);
+    }
+    checkPosture(group);
+    // console.log(group)
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className +
+            ": " +
+            prediction[i].probability.toFixed(2);
+        // labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
+
+    // finally draw the poses
+    drawPose(pose);
+}
+
+function notifyMe(message) {
+    // Let's check if the browser supports notifications
+    const startover = function() {
+        // console.log('NOTIFICATION CLICKed')
+        toggle = true;
+        // console.log(toggle)
+        window.requestAnimationFrame(loop);
+        group = [];
+    };
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var title = "Posture";
+        var icon =
+            "https://homepages.cae.wisc.edu/~ece533/images/airplane.png";
+        var body =
+            message + ". Please sit in correctly and click this notification";
+        var tag = "pose-bot";
+        var notification = new Notification(title, {
+            body,
+            icon,
+            tag
+        });
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(function(permission) {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                var notification = new Notification(title, {
+                    body,
+                    icon,
+                    tag
+                });
+            }
+        });
+    }
+
+    notification.onclick = startover;
+    notification.onclose = startover;
+
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them any more.
+}
+
+function checkPosture(posturegroup) {
+    //group
+    console.log(group);
+    goodposture = (
+        posturegroup.reduce((sum, data) => {
+            return sum + data[0].probability;
+        }, 0) / 100
+    ).toFixed(3);
+    badposture = (
+        posturegroup.reduce((sum, data) => {
+            return sum + data[1].probability;
+        }, 0) / 100
+    ).toFixed(3);
+    nearscreen = (
+        posturegroup.reduce((sum, data) => {
+            return sum + data[2].probability;
+        }, 0) / 100
+    ).toFixed(3);
+
+    console.log('good: ', goodposture, ' bad: ', badposture)
+        // document.getElementById('good').innerHTML = 'Good: ' + goodposture
+        // document.getElementById('bad').innerHTML = 'Bad: ' + badposture
+
+    console.log({
+        goodposture,
+        badposture,
+        nearscreen
+    });
+
+    if (badposture >= 0.9) {
+        // console.log('bad posture')
+        toggle = false;
+        group = [];
+        console.log("Correct your posture");
+    }
+
+    if (nearscreen >= 0.9) {
+        // console.log('bad posture')
+        toggle = false;
+        group = [];
+        console.log("get away from screen");
+    }
+}
+
+function drawPose(pose) {
+    if (webcam.canvas) {
+        ctx.drawImage(webcam.canvas, 0, 0);
+        // draw the keypoints and skeleton
+        if (pose) {
+            const minPartConfidence = 0.5;
+            tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+            tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+        }
+    }
+}
+
+export async function processFrames() {
+    init();
+}
